bc3d2f22e9f086d25656272e2af132d1c6dc8050
Sara Gomes <sarah.gomes80@hotmail.com>
2022-12-04 12:32:53 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
-import { Header } from "./components/Header.jsx";
+
+import styles from "./App.module.css";
 
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
-function App() {
+export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Sara Gomes"
-        content="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-      />
-      <Post author="Fulano de tal" content="Um novo post mt legal" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Sara Gomes"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+          />
+          <Post author="Fulano de tal" content="Um novo post mt legal" />
+        </main>
+      </div>
     </div>
   );
 }
-
-export default App;
