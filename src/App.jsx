0cb3044ac9a380428abdae5d5771b77ca5ee0287
Sara Gomes <sarah.gomes80@hotmail.com>
2022-12-04 10:47:04 -0300

    Iniciando com React - Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { Post } from "./Post";
 function App() {
   return (
     <>
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Sara Gomes"
+        content="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+      />
+      <Post author="Fulano de tal" content="Um novo post mt legal" />
     </>
   );
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,3 +1,10 @@
-export function Post() {
-  return <p>Post</p>;
+// props {author: "", content: ""}
+
+export function Post(props) {
+  return (
+    <div>
+      <strong>{props.author}</strong>
+      <p>{props.content}</p>
+    </div>
+  );
 }
