998612e768e5adce4ac65743365771eddee2b801
Sara Gomes <sarah.gomes80@hotmail.com>
2022-12-04 11:06:00 -0300

    Estrutura da aplicação - CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header.jsx";
+
+import "./styles.css";
 
 function App() {
   return (
-    <>
+    <div>
+      <Header />
+
       <Post
         author="Sara Gomes"
         content="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
       />
       <Post author="Fulano de tal" content="Um novo post mt legal" />
-    </>
+    </div>
   );
 }
 
