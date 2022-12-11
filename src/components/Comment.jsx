b9162e4213401d948dd846f321db1e809dd8675d
Sara Gomes <sarah.gomes80@hotmail.com>
2022-12-11 18:49:24 -0300

    Os motores do React - Comunicação entre componentes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,7 +4,11 @@ import styles from "./Comment.module.css";
 
 import { ThumbsUp, Trash } from "phosphor-react";
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar
@@ -22,7 +26,7 @@ export function Comment({ content }) {
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
