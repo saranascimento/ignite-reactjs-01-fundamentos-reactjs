ea1b6dbf353993e51f7bddd893d7bfefb8cbdeed
Sara Gomes <sarah.gomes80@hotmail.com>
2023-02-05 10:57:21 -0300

    Aprimorando a aplicação - TypeScript no React

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+import ignitLogo from "../assets/ignite-logo.svg";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={ignitLogo} alt="Logotipo do Ignite" />
+    </header>
+  );
+}
